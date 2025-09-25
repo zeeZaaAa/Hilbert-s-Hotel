b5395ef45286ee37021c382758a5d49c7d961bc4
zeeZaaAa <supp.chakorn@gmail.com>
2025-09-25 20:54:08 +0700

    comment icy file

--- icy/duty.py
+++ icy/duty.py
@@ -21,90 +21,90 @@ def load_from_json(filename: str) -> Any:
     return data
 
 # ------------------------------------------------------------
-guests = [
-    {"channel_id": 1, "seq_in_channel": 1, "room_number": 1},
-    {"channel_id": 1, "seq_in_channel": 2, "room_number": 2},
-    {"channel_id": 2, "seq_in_channel": 1, "room_number": 3},
-]
+# guests = [
+#     {"channel_id": 1, "seq_in_channel": 1, "room_number": 1},
+#     {"channel_id": 1, "seq_in_channel": 2, "room_number": 2},
+#     {"channel_id": 2, "seq_in_channel": 1, "room_number": 3},
+# ]
 
-json_filename = "roomData.json"
+# json_filename = "roomData.json"
 
-save_to_json(guests, json_filename)
-loaded_guests = load_from_json(json_filename)
+# save_to_json(guests, json_filename)
+# loaded_guests = load_from_json(json_filename)
 
-print("---- ข้อมูลจาก JSON ----")
-for g in loaded_guests:
-    print(g)
+# print("---- ข้อมูลจาก JSON ----")
+# for g in loaded_guests:
+#     print(g)
 
 # ------------------------------------------------------------
 # time, memory
-timing_records: List[Dict[str, Any]] = []
+# timing_records: List[Dict[str, Any]] = []
 
-def timed(func: Callable = None, *, record_args: bool = True):
-    if func is None:
-        return lambda f: timed(f, record_args=record_args)
+# def timed(func: Callable = None, *, record_args: bool = True):
+#     if func is None:
+#         return lambda f: timed(f, record_args=record_args)
 
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        tracemalloc_started = tracemalloc.is_tracing()
-        if not tracemalloc_started:
-            tracemalloc.start()
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         tracemalloc_started = tracemalloc.is_tracing()
+#         if not tracemalloc_started:
+#             tracemalloc.start()
 
-        wall_start = time.perf_counter()
-        cpu_start = time.process_time()
-        try:
-            result = func(*args, **kwargs)
-            return result
-        finally:
-            wall_end = time.perf_counter()
-            cpu_end = time.process_time()
-            current, peak = tracemalloc.get_traced_memory()
-            if not tracemalloc_started:
-                tracemalloc.stop()
+#         wall_start = time.perf_counter()
+#         cpu_start = time.process_time()
+#         try:
+#             result = func(*args, **kwargs)
+#             return result
+#         finally:
+#             wall_end = time.perf_counter()
+#             cpu_end = time.process_time()
+#             current, peak = tracemalloc.get_traced_memory()
+#             if not tracemalloc_started:
+#                 tracemalloc.stop()
 
-            rec = {
-                "function": func.__name__,
-                "wall_seconds": wall_end - wall_start,
-                "mem_current_bytes": current,
-            }
-            timing_records.append(rec)
-    return wrapper
+#             rec = {
+#                 "function": func.__name__,
+#                 "wall_seconds": wall_end - wall_start,
+#                 "mem_current_bytes": current,
+#             }
+#             timing_records.append(rec)
+#     return wrapper
 
-def deep_getsizeof(obj: Any, ids: set = None) -> int:
-    if ids is None:
-        ids = set()
-    obj_id = id(obj)
-    if obj_id in ids:
-        return 0
-    ids.add(obj_id)
-    size = sys.getsizeof(obj)
-    if isinstance(obj, dict):
-        for k, v in obj.items():
-            size += deep_getsizeof(k, ids)
-            size += deep_getsizeof(v, ids)
-    elif isinstance(obj, (list, tuple, set, frozenset)):
-        for item in obj:
-            size += deep_getsizeof(item, ids)
-    return size
+# def deep_getsizeof(obj: Any, ids: set = None) -> int:
+#     if ids is None:
+#         ids = set()
+#     obj_id = id(obj)
+#     if obj_id in ids:
+#         return 0
+#     ids.add(obj_id)
+#     size = sys.getsizeof(obj)
+#     if isinstance(obj, dict):
+#         for k, v in obj.items():
+#             size += deep_getsizeof(k, ids)
+#             size += deep_getsizeof(v, ids)
+#     elif isinstance(obj, (list, tuple, set, frozenset)):
+#         for item in obj:
+#             size += deep_getsizeof(item, ids)
+#     return size
 
 # ------------------------------------------------------------
 #  test time and memory
-@timed
-def make_numbers(n: int) -> List[int]:
-    return list(range(n))
+# @timed
+# def make_numbers(n: int) -> List[int]:
+#     return list(range(n))
 
-@timed
-def sum_numbers(lst: List[int]) -> int:
-    return sum(lst)
+# @timed
+# def sum_numbers(lst: List[int]) -> int:
+#     return sum(lst)
 
 # ------------------------------------------------------------
-if __name__ == "__main__":
-    nums = make_numbers(1_000_000)
-    total = sum_numbers(nums)
+# if __name__ == "__main__":
+#     nums = make_numbers(1_000_000)
+#     total = sum_numbers(nums)
 
-    print("Total:", total)
-    print("\nTiming records:")
-    for rec in timing_records:
-        print(rec)
+#     print("Total:", total)
+#     print("\nTiming records:")
+#     for rec in timing_records:
+#         print(rec)
 
-    print("\nApprox memory usage of nums:", deep_getsizeof(nums), "bytes")
+#     print("\nApprox memory usage of nums:", deep_getsizeof(nums), "bytes")
