fae038a0137d559647dbebdbe6e5cfa8789dc0a4
Nathan <nathanol.pick@gmail.com>
2025-09-29 22:16:31 +0700

    created add function

--- pix/util/gen_room_num.py
+++ pix/util/gen_room_num.py
@@ -1,1 +1,19 @@
-#test
+def add(db :dict, list_of_guest :list):
+    if  not isinstance(db, dict):
+        return "Error, db must be a dict"
+    
+    if not isinstance(list_of_guest, list):
+        return "Error, list_of_guest must be a list"
+
+    num_to_shift = len(list_of_guest)
+    current_keys = list(db.keys())
+    for key in current_keys:
+        old_guest = db[key]
+        db[key+num_to_shift] = old_guest
+        db.pop(key)
+
+    room_num = 1
+    for guest in list_of_guest:
+        db[room_num] = guest
+        room_num+=1
+    return db
