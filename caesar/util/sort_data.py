351dd1a4ceed5388416a3b8cb8c9e0a46a634104
zeeZa <67010199@kmitl.ac.th>
2025-09-30 20:50:22 +0700

    fix sort

--- caesar/util/sort_data.py
+++ caesar/util/sort_data.py
@@ -33,7 +33,7 @@ def merge_sort_iterative(arr):
 
 def sort_data(db: dict):
     try:
-        items = db.items()
+        items = list(db.items())
         sorted_items = merge_sort_iterative(items)
         return dict(sorted_items)
     except ValueError as e:
